66673c3d552d83f2f235d767087b462948c8efbb
Kevin <kevstar421@gmail.com>
2021-05-23 18:25:32 -0400

    Tap out of modal
    Allows touchscreen users to tap out of the filter modal

--- scripts/modal.js
+++ scripts/modal.js
@@ -23,3 +23,8 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+window.ontouchstart = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
