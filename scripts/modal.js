8edb957018cf409150d6b6ae0eba17505b48a894
Kevin <kevstar421@gmail.com>
2021-09-01 21:25:08 -0400

    modal JS optimization
    Removed unnecessary braces & return keyword from arrow functions

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,22 +1,14 @@
-const $ = (selector) => {
-  return document.querySelector(selector);
-};
+const $ = selector => document.querySelector(selector);
 
 // Function to display modal when button is clicked
-const displayModal = () => {
-  document.querySelector("#myBtnModal").style.display = "block";
-};
+const displayModal = () => document.querySelector("#myBtnModal").style.display = "block";
 $("#myBtn").addEventListener("click", displayModal);
 
 
 // Function to close modal when "x" is clicked
-const closeModal = () => {
-  document.querySelector("#myBtnModal").style.display = "none";
-};
+const closeModal = () => document.querySelector("#myBtnModal").style.display = "none";
 $(".closeBtn").addEventListener("click", closeModal);
 
 // Function to close modal when clicked outside
-const closeModalOutside = (target) => {
-  document.querySelector("#myBtnModal").style.display = "none";
-};
+const closeModalOutside = target => document.querySelector("#myBtnModal").style.display = "none";
 $("#myBtnModal").addEventListener("click", closeModalOutside);
