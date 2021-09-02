1cdd069c272045cc8ef2a7adfea01b1d75b4cc65
Kevin <kevstar421@gmail.com>
2021-09-01 20:46:36 -0400

    Modal JS update
    Updated javascript to open the modal

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,30 +1,22 @@
-// Get the modal
-var modal = document.getElementById("myBtnModal");
+const $ = (selector) => {
+  return document.querySelector(selector);
+};
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+// Function to display modal when button is clicked
+const displayModal = () => {
+  document.querySelector("#myBtnModal").style.display = "block";
+};
+$("#myBtn").addEventListener("click", displayModal);
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("closeBtn")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+// Function to close modal when "x" is clicked
+const closeModal = () => {
+  document.querySelector("#myBtnModal").style.display = "none";
+};
+$("#closeBtn").addEventListener("click", closeModal);
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-window.ontouchmove = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+// Function to close modal when clicked outside
+const closeModalOutside = (target) => {
+  document.querySelector("#myBtnModal").style.display = "none";
+};
+$("#myBtnModal").addEventListener("click", closeModalOutside);
