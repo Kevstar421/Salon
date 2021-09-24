b0c5e6df6db98aa7646f6f8090c9201d5760c48e
Kevin <kevstar421@gmail.com>
2021-09-23 20:19:41 -0400

    MODAL TABLOCK
    Added tab locking to the filter modal.
    Prevents everything but the modal's elements from being tab accessible

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,20 +1,69 @@
+"use strict";
+
 const $ = selector => document.querySelector(selector);
+const everyElement = document.querySelectorAll("*");
+const modalLinkElements = document.querySelectorAll("#myBtnContainer p a");
 
 document.addEventListener("DOMContentLoaded", () => {
     // Function to display modal when button is clicked
     $("#myBtn").addEventListener("click", () => {
         $("#myBtnModal").style.display = "block";
+
+        // Prevents tab access to every element
+        for ( let i = 0; i < everyElement.length; i++ ) {
+            everyElement[i].setAttribute("tabindex", "-1");
+        }
+
+        // Prevents page from being scrollable inside the modal
+        $("body").style.overflow = "hidden";
+
+        // Allows elements in modal to be tab accessible
+        for ( let i = 0; i < modalLinkElements.length; i++ ) {
+            $(".closeBtn").setAttribute("tabindex", "0");
+            modalLinkElements[i].setAttribute("tabindex", "0");
+        }
     });
 
     // Function to close modal when "x" is clicked
     $(".closeBtn").addEventListener("click", () => {
         $("#myBtnModal").style.display = "none";
+
+        // Allows tab access
+        for ( let i = 0; i < everyElement.length; i++ ) {
+            everyElement[i].setAttribute("tabindex", "");
+        }
+
+        // Allows page to be scrollable
+        $("body").style.overflow = "initial";
     });
 
     // Function to close modal when clicked outside
-    $("#myBtnModal").addEventListener("click", pointer => {
-        if ( pointer.target == $("#myBtnModal") ) {
+    $("#myBtnModal").addEventListener("click", evt => {
+        if ( evt.target == $("#myBtnModal") ) {
             $("#myBtnModal").style.display = "none";
+
+            // Allows tab access
+            for ( let i = 0; i < everyElement.length; i++ ) {
+                everyElement[i].setAttribute("tabindex", "");
+            }
+
+            // Allows page to be scrollable
+            $("body").style.overflow = "initial";
+        }
+    });
+
+    // Allows modal to be closed via keyboard
+    $(".closeBtn").addEventListener("keyup", evt => {
+        if ( evt.keyCode == 13 || evt.keyCode == 32 ) {
+            $("#myBtnModal").style.display = "none";
+            
+            // Allows tab access
+            for ( let i = 0; i < everyElement.length; i++ ) {
+                everyElement[i].setAttribute("tabindex", "");
+            }
+
+            // Allows page to be scrollable
+            $("body").style.overflow = "initial";
         }
     });
 });
