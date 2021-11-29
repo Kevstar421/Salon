f12cf66a5cc569be8d50716396877f6d5a2a5a38
Kevstar421 <kevstar421@gmail.com>
2021-11-29 18:00:39 -0500

    Modal Animation & Video PB
    Added a closing animation for the filer modal.
    Added a play button for the videos

--- scripts/modal.js
+++ scripts/modal.js
@@ -4,10 +4,43 @@ const $ = selector => document.querySelector(selector);
 const everyElement = document.querySelectorAll("*");
 const modalLinkElements = document.querySelectorAll("#myBtnContainer p a");
 
+const animationFinished = () => $("#myBtnModal").style.display = "none";
+
+const closeModal = evt => {
+    if ( evt.target == $("#myBtnModal") || evt.target == $(".closeBtn") ) {
+        // Prevents  setTimeout delay from resetting due to spam clicking outside the modal
+        $("#myBtnModal").removeEventListener("click", closeModal);
+
+        $("#myBtnModal").style.backgroundColor = "transparent";
+
+        // Closing animation
+        $(".modalBtnContent").style.webkitAnimationName = "animatebottom";
+        $(".modalBtnContent").style.animationName = "animatebottom";
+
+        setTimeout(animationFinished, 395);
+
+        // Allows tab access
+        for ( let i = 0; i < everyElement.length; i++ ) {
+            everyElement[i].setAttribute("tabindex", "");
+        }
+
+        // Allows page to be scrollable
+        $("body").style.overflow = "initial";
+    }
+};
+
 document.addEventListener("DOMContentLoaded", () => {
     // Function to display modal when button is clicked
     $("#myBtn").addEventListener("click", () => {
+        $("#myBtnModal").addEventListener("click", closeModal);
+
+        $("#myBtnModal").style.backgroundColor = "rgba(0, 0, 0, 0.4)";
+
         $("#myBtnModal").style.display = "block";
+        
+        // Opening animation
+        $(".modalBtnContent").style.webkitAnimationName = "animatetop";
+        $(".modalBtnContent").style.animationName = "animatetop";
 
         // Prevents tab access to every element
         for ( let i = 0; i < everyElement.length; i++ ) {
@@ -25,48 +58,15 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     // Function to close modal when "x" is clicked
-    $(".closeBtn").addEventListener("click", () => {
-        $("#myBtnModal").style.display = "none";
-
-        // Allows tab access
-        for ( let i = 0; i < everyElement.length; i++ ) {
-            everyElement[i].setAttribute("tabindex", "");
-        }
-
-        // Allows page to be scrollable
-        $("body").style.overflow = "initial";
-    });
+    $(".closeBtn").addEventListener("click", closeModal);
 
     // Function to close modal when clicked outside
-    $("#myBtnModal").addEventListener("click", evt => {
-        if ( evt.target == $("#myBtnModal") ) {
-            $("#myBtnModal").style.display = "none";
-
-            // Allows tab access
-            for ( let i = 0; i < everyElement.length; i++ ) {
-                everyElement[i].setAttribute("tabindex", "");
-            }
-
-            // Allows page to be scrollable
-            $("body").style.overflow = "initial";
-        }
-    });
+    $("#myBtnModal").addEventListener("click", closeModal);
 
     // Allows modal to be closed via keyboard
     $(".closeBtn").addEventListener("keyup", evt => {
         if ( evt.keyCode == 13 || evt.keyCode == 32 ) {
-            $("#myBtnModal").style.display = "none";
-            
-            // Allows tab access
-            for ( let i = 0; i < everyElement.length; i++ ) {
-                everyElement[i].setAttribute("tabindex", "");
-            }
-
-            // Allows page to be scrollable
-            $("body").style.overflow = "initial";
+            closeModal(evt);
         }
     });
 });
-
-
-
